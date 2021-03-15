80b6d4d154008ee3f68ec57ca0250178afe20843
jimmydragneel14 <jimmymokz14@gmail.com>
2021-03-14 18:10:18 -0600

    Functions for normalize

--- Indexacion.py
+++ Indexacion.py
@@ -1,5 +1,6 @@
 from os import walk
-import math 
+import math
+import re
 
 def indexColection():
 
@@ -9,12 +10,13 @@ def indexColection():
     cont_files = 0
     documents = {}
     vocabulary = {}
+    stopwords = read_stopwords()
     
     for (dirpath, dirs, files) in walk(root):
         relative_path='.'+ dirpath.split(root)[-1] + '\\'
         print(relative_path)
         for file in files:
-            with open(dirpath+'\\'+file, "r") as file:
+            with open(dirpath+'\\'+file, "rt", encoding='utf8') as file:
                 documents[cont_files]= {'path': relative_path,'name':file.name.split("\\")[-1], 'pairs': {}}
                 cont_words = 0
                 cont_description_words=0
@@ -68,6 +70,86 @@ def calculate_idfi(vocabulary,N):
             vocabulary[word]['idfi']=0
         else:
             vocabulary[word]['idfi'] = math.log((N-vocabulary[word]['n_i']-0.5)/(vocabulary[word]['n_i']-0.5),2)
-    
+
+###########  Normalize #################
+def normalize(text):
+    #Funcion: este metodo sirve para normalizar el texto que se recibe.
+    #Parametros: String.
+    #Resultado: String normalizado de acuerdo a los criterios.
+
+    text_lower = text.lower()
+    text_unaccent = remove_accent(text_lower)
+    return text_unaccent  
+
+def remove_accent(text):
+    #Funcion: este metodo sirve para remover tildes y dieresis.
+    #Parametros: String.
+    #Resultado: String sin acentos.
+
+    replacements = (
+        ["á", "a"],
+        ["é", "e"],
+        ["í", "i"],
+        ["ó", "o"],
+        ["ú", "u"],
+        ["ä", "a"],
+        ["ë", "e"],
+        ["ï", "i"],
+        ["ö", "o"],
+        ["ü", "u"]
+    )
+    for accent_letter, letter in replacements:
+        
+        text = text.replace(accent_letter,letter)
+
+    return text
+
+def read_stopwords():
+    #Funcion: este metodo carga y enlista los stopwords.
+    #Parametros: No hay.
+    #Resultado: Lista de stopwords
+    stopwords = []
+
+    with open('stopwords.txt') as f:
+        for line in f:
+            stopwords.append(line.split()[0])
+    return stopwords
+
+
+def check_point(word):
+    #Funcion: este metodo revisa los terminos con el signo punto.
+    #Parametros: Palabra o string
+    #Resultado: Booleano
+    if word[0] == "." or word[-1] == ".":
+        return False
+    if ".." in word:
+        return False
+    return True
+        
+def double_line(word):
+    #Funcion: Maneja elm caso "--".
+    #Parametros: Palabra o string
+    #Resultado: Lista con terminos
+    if word[0] == "-" and word[1] == "-":
+        return [word[2:],"@" + word[2:]]
+
+
+def splitpoints(word):
+    #Funcion: Divide las palabras con puntos y eliminha los que son solo numeros.
+    #Parametros: Palabra o string
+    #Resultado: Lista con terminos.
+
+    x = re.search("[a-zA-Z]", word)
+    result = []
+    if "." in word:
+        if x:
+            word_splited = word.split(".")
+    for i in word_splited:
+        if i.isnumeric()==False:
+            result.append(i)
+    result.append(word)
+    return result
+
+#####################################
 
 indexColection()
